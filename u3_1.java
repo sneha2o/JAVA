05e12fd8496603500506f0b5fee82e14af84f6ff
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-01-16 18:34:00 +0530

    university practical's

---
+++ u3_1.java
@@ -0,0 +1,17 @@
+import p.u3_0;
+
+public class u3_1 //package class 
+{
+	public static void main(String args[])
+	{
+		u3_0 p1=new u3_0();
+		
+		int ans=p1.findmax(99,55,26);
+		
+		System.out.println("Maximun Number=>"+ans);
+	}
+}
+
+//javac -d . u3_0.java
+//javac u3_1.java
+//java u3_1
